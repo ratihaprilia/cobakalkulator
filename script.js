6341cad556a1031155f0434a298e8ef482cb3c34
ratihaprilia <67065430+ratihaprilia@users.noreply.github.com>
2020-07-14 22:49:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,66 @@
-const numbers = document.querySelectorAll(".number")
-
-numbers.forEach((number) => {
-  number.addEventListener("click", () => {
-    console.log("numbers is pressed")
-  })
-})
-
 const calculatorScreen = document.querySelector('.calculator-screen')
 
 const updateScreen = (number) => {
   calculatorScreen.value = number
+}
+
+const numbers = document.querySelectorAll(".number")
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    updateScreen(event.target.value)
+  })
+})
+
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
+
+const inputNumber = (number) => {
+  if (currentNumber === '0') {
+    currentNumber = number
+  } else {
+    currentNumber += number
+  }
+}
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    inputNumber(event.targer.value)
+    updateScreen(currentNumber)
+  })
+})
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value)
+  })
+})
+
+const inputOperator = (operator) => {
+  prevNumber = currentNumber
+  calculationOperator = operator
+  currentNumber = ''
+}
+
+const equalSign = document.querySelectorAll('.equal-sign')
+
+equalSign.addEventListener('click', () => {
+    calculate() 
+    updateScreen(currentNumber)
+  })
+
+const calculate = () => {
+   let result = ''
+   switch(calculationOperator) {
+     case '+':
+       result = prevNumber + currentNumber break
+     case '-':
+       result = prevNumber - currentNumber break
+    }
+    currentNumber = result
+    calculatinOperator = ''
+     
 }
